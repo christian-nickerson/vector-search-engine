3ee7e9e665594740a73d1dfddd0bba056619870b
christian nickerson <christian_nickerson@hotmail.com>
2023-06-28 15:23:26 +0100

    exception handling added

--- src/data/data.py
+++ src/data/data.py
@@ -11,9 +11,12 @@ class NikeDataset:
     """Class for getting Nike Dataset"""
 
     def __init__(self) -> None:
-        kaggle.api.authenticate()
-        kaggle.api.dataset_download_files(settings.data.dataset_name, path=DATA_PATH, unzip=True)
-        self._df = pd.read_csv("src/data/nike_data_2022_09.csv")
+        try:
+            kaggle.api.authenticate()
+            kaggle.api.dataset_download_files(settings.data.dataset_name, path=DATA_PATH, unzip=True)
+            self._df = pd.read_csv("src/data/nike_data_2022_09.csv")
+        except Exception as e:
+            raise e
 
     @property
     def df(self) -> pd.DataFrame:
