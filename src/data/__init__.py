a137bc7f614ec55ca569ea66ab51055191b7d035
christian nickerson <christian_nickerson@hotmail.com>
2023-07-11 19:15:33 +0100

    secrets added

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -2,7 +2,9 @@ import os
 
 from config import settings
 
-os.environ["KAGGLE_USERNAME"] = settings.kaggle.username
-os.environ["KAGGLE_KEY"] = settings.kaggle.key
+if os.getenv("KAGGLE_USERNAME") is None:
+    os.environ["KAGGLE_USERNAME"] = settings.kaggle.username
+if os.getenv("KAGGLE_KEY") is None:
+    os.environ["KAGGLE_KEY"] = settings.kaggle.key
 
 from data.data import NikeDataset  # noqa: E402
