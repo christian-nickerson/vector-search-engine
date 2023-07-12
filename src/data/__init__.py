7fdd05c7cad220c943a55aab26354d720b6f26f5
christian nickerson <christian_nickerson@hotmail.com>
2023-07-12 10:20:22 +0100

    testing ci commit

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -3,6 +3,7 @@ import os
 from config import settings
 
 if os.getenv("KAGGLE_USERNAME") is None:
+    raise Exception("No api vars")
     os.environ["KAGGLE_USERNAME"] = settings.kaggle.username
 if os.getenv("KAGGLE_KEY") is None:
     os.environ["KAGGLE_KEY"] = settings.kaggle.key
