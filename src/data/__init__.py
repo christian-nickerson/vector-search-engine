109453dd2947a43b950641d39faaa8a3fb080ba8
christian nickerson <christian_nickerson@hotmail.com>
2023-07-12 18:01:17 +0100

    testing file fix

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -2,9 +2,7 @@ import os
 
 from config import settings
 
-if os.getenv("KAGGLE_USERNAME") is None:
-    os.environ["KAGGLE_USERNAME"] = settings.kaggle.username
-if os.getenv("KAGGLE_KEY") is None:
-    os.environ["KAGGLE_KEY"] = settings.kaggle.key
+os.environ["KAGGLE_USERNAME"] = settings.kaggle.username
+os.environ["KAGGLE_KEY"] = settings.kaggle.key
 
 from data.data import NikeDataset  # noqa: E402
